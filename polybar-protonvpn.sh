7800ef6305d667dc63f43603c766ee1052830de9
Nikhil Nawgiri <the@realest.ninja>
2021-09-05 13:22:19 +0200

    Adjust to changes in protonvpn-cli

--- polybar-protonvpn.sh
+++ polybar-protonvpn.sh
@@ -2,7 +2,7 @@
 
 status=$(protonvpn s | awk '{print $2}')
 
-connection_status=$(awk 'FNR == 1 {print}' <<< $status)
+connection_status=$(awk 'FNR == 4 {print}' <<< $status)
 
 if [ "$connection_status" = 'Connected' ]
 then
@@ -15,8 +15,8 @@ then
   tmpfile=$(mktemp /tmp/$cache_filename.XXXXX)
   echo "$status" > $tmpfile
 
-  server=$(awk 'FNR == 4 {print}' <<< $status)
-  emoji=$([ $(awk 'FNR == 5 {print}' <<< $status) == "Secure-Core" ] && echo "🔐" || echo "🔒")
+  server=$(awk 'FNR == 7 {print}' <<< $status)
+  emoji=$([ $(awk 'FNR == 8 {print}' <<< $status) == "Secure-Core" ] && echo "🔐" || echo "🔒")
   echo "$emoji $server"
 else
   echo ""
