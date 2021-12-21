2cb60ee100568073cb42f98704531f61c0088fee
Joseph Diza <josephm.diza@gmail.com>
2021-12-21 14:58:38 -0800

    Add nullchecks for script

--- polybar-protonvpn.sh
+++ polybar-protonvpn.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 tmpfile="protonvpn-status-cache"
+[[ -e "/usr/bin/protonvpn-cli" ]] &&  exit 1 || :
 connection_status=$(protonvpn-cli s | awk '{print $2}' | grep "Connection")
+[[ $connection_status -eq 1 ]] && exit 1 || :
 if [ "$connection_status" == 'Connection' ]; then
     server=$(protonvpn-cli s | grep "Server:" | tr -d ' ' | awk '{print $2}')
     echo "$(protonvpn-cli s)" > /tmp/$tmpfile
